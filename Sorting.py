384823df45528c851785b28a79a168ef971f0860
16kbf@queensu.ca <33009041+kaiferrall@users.noreply.github.com>
2018-11-10 00:48:40 -0500

    Quick change to bubble sort

--- Sorting.py
+++ Sorting.py
@@ -1,7 +1,6 @@
-#bubble sort
-def bubble_sort(array):
-    for j in range(0, len(array)-1):
-        for i in range(0, len(array)-1):
+def bubble_sort( array ):
+    for j in range(0, len(array)-2):
+        for i in range(0, len(array)-1-j):
             if array[i] > array[i+1]:
                 placeholder = array[i+1]
                 array[i+1] = array[i]
@@ -23,6 +22,6 @@ def selection_sort(array):
     return array
 
 
-a = [1, 3, 45, 29, 29, 9, 10, 100, 1010]
+a = [1, 3, 31231, 45, 29, 1010, 29, 9, 10, 100, 89, 2031, 2312, 890]
 print(selection_sort(a))
 print(bubble_sort(a))
