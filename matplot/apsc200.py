fd8bb1d7fc527881e082f71adf3ef4a7e36225aa
16kbf@queensu.ca <33009041+kaiferrall@users.noreply.github.com>
2018-11-29 01:51:56 -0500

    Added 2d scatter plot with infectivity displayed by color

--- matplot/apsc200.py
+++ matplot/apsc200.py
@@ -3,11 +3,11 @@ import matplotlib.pyplot as plt
 import pandas as pd
 import numpy as np
 
-
 def setData(col):
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
     ax.set_zlabel('Infectivity')
+    ax.set_zlim(0, 1)
     ax.set_xlabel('X-Position')
     ax.set_ylabel('Y-Position')
 
@@ -22,8 +22,31 @@ def setData(col):
     plt.savefig("plot_" + str(col) + ".png")
 
 
-for i in range(0, 49):
-    if i % 2 == 0:
-        setData(i)
+def setData_2D(col):
+    fig = plt.figure()
+    colors = ["#c62727" for i in range(0, 100)]
+    xpos = pd.read_excel("agentPosition.xlsx", sheet_name="Sheet1",
+                         usecols=[col], nrows=100)
+    ypos = pd.read_excel("agentPosition.xlsx", sheet_name="Sheet1",
+                         usecols=[col+1], nrows=100)
+    area = pd.read_excel("agentData.xlsx", sheet_name="Sheet1",
+                              usecols=[col], nrows=100)*800
+    for i in range(0, 99):
+        if area.at[i, 1] > 0.85:
+            colors[i] = "#d11717"
+        elif area.at[i, 1] > 0.5:
+            colors[i] = "#d15b17"
+        elif area.at[i, 1] > 0.4:
+            colors[i] = "#c66627"
+        elif area.at[i, 1] > 0.3:
+            colors[i] = "#dfe22d"
+        elif area.at[i, 1] > .15:
+            colors[i] = "#17d1ab"
+        else:
+            colors[i] = "#17d158"
 
-plt.show()
+    plt.scatter(xpos, ypos, s=50, c=colors, alpha=0.65)
+    plt.savefig("plot_" + str(col) + ".png")
+
+for i in range(0, 50):
+    setData_2D(i)
